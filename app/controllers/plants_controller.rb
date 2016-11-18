e0638a94effa0cb8ea39507dfde6d33cd2b719a6
Laura Pedroni <laurage_@hotmail.com>
2016-11-18 21:51:06 +0100

    create action works

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,21 +1,21 @@
 class PlantsController < ApplicationController
   before_action :find_plant, only:[:show]
-  before_action :find_user, only:[:new, :create]
+  before_action :find_user, only:[]
 
   def show
   end
 
   def new
     @plant = Plant.new
-    raise
   end
 
   def create
-    # @plant = Plant.new(plant_params)
-    # @plant.save
-    # @ownership = Ownership.new(plant: @plant, gardener: @gardener)
-    # @ownership.save
-    # redirect_to plant_path(@plant)
+    @plant = Plant.new(plant_params)
+    @plant.save
+
+    @ownership = Ownership.new(plant: @plant, user: current_user)
+    @ownership.save
+    redirect_to plant_path(@plant)
   end
 
   def edit
@@ -27,8 +27,8 @@ class PlantsController < ApplicationController
     @plant = Plant.find(params[:id])
   end
 
-  def find_gardener
-    @user = User.find(params[:gardener_id])
+  def find_user
+    @user = User.find(params[:user_id])
   end
 
   def plant_params
