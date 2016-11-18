50c33da200f96be80d77ce7ca6a8bc78695b627e
Laura Pedroni <laurage_@hotmail.com>
2016-11-18 01:15:18 +0100

    filling form new

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,6 @@
 class PlantsController < ApplicationController
-  before_action :set_plant, only:[:show]
+  before_action :find_plant, only:[:show]
+  before_action :find_gardener, only:[:new]
 
   def show
   end
@@ -8,10 +9,29 @@ class PlantsController < ApplicationController
     @plant = Plant.new
   end
 
+  def create
+    @gardener = Gardener.find(params[:gardener_id])
+    @plant = Plant.new(plant_params)
+    @plant.save
+    @ownership = Ownership.new(plant: @plant, gardener: @gardener)
+    @ownership.save
+    redirect_to plant_path(@plant)
+  end
+
   def edit
   end
 
-  def set_plant
+  private
+
+  def find_plant
     @plant = Plant.find(params[:id])
+  end
+
+  def find_gardener
+    @gardener = Gardener.find(params[:gardener_id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:name)
   end
 end
