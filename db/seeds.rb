a524e7565fd951870c444bd575f88c3af21e7ae6
Laura Pedroni <laurage_@hotmail.com>
2017-01-19 22:12:01 +0100

    n

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,5 @@ o1 = Ownership.create(user: g1, plant: p1, current_owner: true)
 o2 = Ownership.create(user: g2, plant: p2, current_owner: true)
 o3 = Ownership.create(user: g2, plant: p3, current_owner: true)
 
-
+#test: pass all ownerships as not current owner, see if transformed
+#Problem observable on user profile
